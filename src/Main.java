992102938443f4f0c8e153a34662e63d94715dcf
ViniciusFreiry <viniciusfreiry@gmail.com>
2025-05-26 12:33:16 -0300

    Calculating Check-Out

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.time.Duration;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
@@ -9,12 +10,21 @@ public class Main {
 
         // User Inputs
         System.out.println("Enter your clock-in time (HH:mm):");
-        LocalTime entry = LocalTime.parse(input.nextLine(), formatter);
+        LocalTime entry = LocalTime.parse(input.next(), formatter);
 
         System.out.println("Enter the daily workload (in hours):");
         int workload = input.nextInt();
 
         System.out.println("Enter the actual clock-out time (HH:mm):");
-        LocalTime exit = LocalTime.parse(input.nextLine(), formatter);
+        LocalTime exit = LocalTime.parse(input.next(), formatter);
+
+        input.close();
+
+        // Calculating difference between actual and expected check-out
+        LocalTime expectedCheckOut = entry.plusHours(workload);
+
+        Duration difference = Duration.between(expectedCheckOut, exit);
+        long overtimeHours = difference.toHours();
+        long overtimeMinutes = difference.toMinutes();
     }
 }
